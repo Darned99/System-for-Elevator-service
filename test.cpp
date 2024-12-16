70536e18d390a8ad1a5a27f72652d5e57d1a03eb
Darned99 <b.kisielewski99@gmail.com>
2024-12-16 15:33:01 +0100

    Test file

---
+++ test.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "elevator.hpp"
+#include "lobby.hpp"
+#include <string>
+#include <vector>
+
+int main(){
+    
+    double number_of_elevators;
+    double number_of_floors;
+
+    std::cout << "Set up program. How many elevator would you like" << std::endl;
+    std::cin << number_of_elevators;
+
+    std::cout << "How many floors are in the building?" << std::endl;
+    std::cin << number_of_floors;
+
+
+
+    std::cout << ""
+
+
+    return 0;
+}
