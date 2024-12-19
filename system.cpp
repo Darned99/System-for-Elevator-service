df0d472e16fe529f761e589fd11bf056e6668b6e
Darned99 <kisielewskibartek99@gmail.com>
2024-12-19 09:55:02 +0100

    Changes

--- system.cpp
+++ system.cpp
@@ -5,14 +5,22 @@
 namespace bk{
     class System : Elevator{
         protected:
-            int id;
+            int m_id;
 
 
         public:
             // Constructor wip
         
         
-
+       bool bk::System::operator==(const bk::System& other) const {
+        return (m_CurrentFloor == other.m_CurrentFloor &&
+        m_MaxFloor == other.m_MaxFloor &&
+        m_MinFloor == other.m_MinFloor);
+    }
+    
+    bool bk::System::operator!=(const bk::System& other) const {
+        return !(*this == other);
+    }
         
 
 
